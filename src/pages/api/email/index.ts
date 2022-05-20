95649765a7e4a1e9d031ffdfeb1cafac52f81231
Icaro Vieira <icarovsilva1@gmail.com>
2022-05-20 20:27:14 -0300

    chagen de post api routes

--- src/pages/api/email/index.ts
+++ src/pages/api/email/index.ts
@@ -6,47 +6,54 @@ export default async function email(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const { message, name } = request.body;
+  const { method } = request;
 
-  if (!message || !name) {
-    return response
-      .status(400)
-      .json({ message: "Error in the mandatory fields" });
+  if (method === "GET") {
+    return response.status(404).json({ message: "Cannot Get" });
   }
 
-  const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "2afbacbf4f74b1",
-      pass: "c31ba0e8d6f7e8",
-    },
-  });
+  if (method === "POST") {
+    const { message, name } = request.body;
 
-  const sendEmail = {
-    email: `Icaro portfolio <${name}>`,
-    body: `<div><b>${message}</b></div>`,
-  };
-
-  try {
-    const data = await transport.sendMail({
-      subject: "Icaro Portfólio",
-      from: sendEmail.email,
-      to: "Icaro Vieira <icarovsilva1@gmail.com>",
-      html: sendEmail.body,
-    });
-
-    const email = await prisma.contacts.create({
-      data: {
-        email: name,
+    if (!message || !name) {
+      return response
+        .status(400)
+        .json({ message: "Error in the mandatory fields" });
+    }
+    const transport = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "2afbacbf4f74b1",
+        pass: "c31ba0e8d6f7e8",
       },
     });
 
-    return response.status(200).json({
-      data,
-      email,
-    });
-  } catch (error) {
-    return response.status(500).json({ message: "Error has ben ocorried" });
+    const sendEmail = {
+      email: `Icaro portfolio <${name}>`,
+      body: `<div><b>${message}</b></div>`,
+    };
+
+    try {
+      const data = await transport.sendMail({
+        subject: "Icaro Portfólio",
+        from: sendEmail.email,
+        to: "Icaro Vieira <icarovsilva1@gmail.com>",
+        html: sendEmail.body,
+      });
+
+      const email = await prisma.contacts.create({
+        data: {
+          email: name,
+        },
+      });
+
+      return response.status(200).json({
+        data,
+        email,
+      });
+    } catch (error) {
+      return response.status(500).json({ message: "Error has ben ocorried" });
+    }
   }
 }
